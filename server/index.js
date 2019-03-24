913fb19a681b1407e867e394ef06cdd3b4bcd93b
tetsuromori <tetsuromori@gmail.com>
2019-03-25 00:19:24 +0900

    add client

--- server/index.js
+++ server/index.js
@@ -34,25 +34,34 @@ const redisClient = redis.createClient({
 const redisPublisher = redisClient.duplicate();
 
 // Express Route Handlers
+
 app.get('/', (req, res) => {
     res.send('Hi')
 });
+
 app.get('/values/all', async (req, res) => {
     const values = await pgClient.query('SELECT * values');
+
     res.send(values.rows)
 });
+
 app.get('/values/current', async (req, res) => {
     redisClient.hgetall('values', (err, values) => {
         res.send(values);
     });
 });
+
 app.post('/values', async (req, res) => {
     const index = req.body.index;
-    if(index > 40) return res.status(422).send('Index too high');
+
+    if(index > 40) {
+        return res.status(422).send('Index too high')
+    };
 
     redisClient.hset('values', index, 'Nothing yet!');
     redisPublisher.publish('insert', index);
     pgClient.query('INSERT INTO values(numer) VALUES($1)', [index]);
+
     res.send({ working: true })
 });
 
